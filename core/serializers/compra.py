3c239b7451dec93956103574055f1ebfe0694829
Gianlucagadotti <gianlucagadotti@gmail.com>
2026-06-23 14:31:39 -0300

    feat: limitando os campos dos itens da compra na listagem de compras

--- core/serializers/compra.py
+++ core/serializers/compra.py
@@ -6,7 +6,7 @@ from core.models import Compra, ItensCompra
 class ItensCompraSerializer(ModelSerializer):
     class Meta:
         model = ItensCompra
-        fields = '__all__'
+        fields = ('livro', 'quantidade')
 class CompraSerializer(ModelSerializer):
     usuario = CharField(source='usuario.email', read_only=True)
     status = CharField(source='get_status_display', read_only=True) # inclua essa linha
