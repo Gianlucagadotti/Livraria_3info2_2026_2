54f332ce1ba26f262e77a737b3fb51c1d1614169
Gianlucagadotti <gianlucagadotti@gmail.com>
2026-08-18 14:59:38 -0300

    feat: Gravação do preço do livro no item da compra

--- core/serializers/compra.py
+++ core/serializers/compra.py
@@ -14,7 +14,7 @@ from core.models import Compra, ItensCompra
 class ItensCompraCreateUpdateSerializer(ModelSerializer):
     class Meta:
         model = ItensCompra
-        fields = ('livro', 'quantidade')
+        fields = ('livro', 'quantidade', 'preco')  # mudou
 
     def validate_quantidade(self, quantidade):
         if quantidade <= 0:
@@ -36,20 +36,23 @@ class CompraCreateUpdateSerializer(ModelSerializer):
 
     @transaction.atomic
     def create(self, validated_data):
-        itens_data = validated_data.pop('itens')
+        itens = validated_data.pop('itens')
         compra = Compra.objects.create(**validated_data)
-        for item_data in itens_data:
-            ItensCompra.objects.create(compra=compra, **item_data)
+        for item in itens:
+            item['preco'] = item['livro'].preco  # preço do livro no momento da compra
+            ItensCompra.objects.create(compra=compra, **item)
         compra.save()
         return compra
 
     @transaction.atomic
     def update(self, compra, validated_data):
-        itens_data = validated_data.pop('itens', None)
-        if itens_data is not None:
+        itens = validated_data.pop('itens')
+        if itens:
             compra.itens.all().delete()
-            for item_data in itens_data:
-                ItensCompra.objects.create(compra=compra, **item_data)
+            for item in itens:
+                item['preco'] = item['livro'].preco  # grava o preço histórico
+                ItensCompra.objects.create(compra=compra, **item)
+        compra.save()
         return super().update(compra, validated_data)
 
 
@@ -58,7 +61,7 @@ class ItensCompraListSerializer(ModelSerializer):
 
     class Meta:
         model = ItensCompra
-        fields = ('quantidade', 'livro')
+        fields = ('quantidade', 'preco', 'livro')  # mudou
         depth = 1
 
 
@@ -79,7 +82,7 @@ class ItensCompraSerializer(ModelSerializer):
 
     class Meta:
         model = ItensCompra
-        fields = ('livro', 'quantidade', 'total', 'preco')
+        fields = ('livro', 'quantidade', 'preco', 'total')  # mudou
         depth = 1
 
 
